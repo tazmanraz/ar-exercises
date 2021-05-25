5f7993b5523f0224753f2f4ebb880d095d0890aa
Imtiaz Miah <imtiazmiah@gmail.com>
2021-05-25 16:26:53 +0000

    Add class to run exercises 6 properly

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,6 +6,6 @@ puts "----------"
 
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
-@store1 = Store.find_by(id: 2)
+@store2 = Store.find_by(id: 2)
 
 @store1.update(name: "Derp")
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,6 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# class Store 
+#   has_many :employees
+# end
+
+# class Employee 
+#   belongs_to :Store
+# end
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Son", last_name: "Goku", hourly_rate: 70)
